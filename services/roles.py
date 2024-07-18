dc4f43ebe90defb99d74413acbacab04e2fb4f48
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-07-18 14:15:59 -0500

    Add assitant role with email and calendar functions

--- services/roles.py
+++ services/roles.py
@@ -1,8 +1,9 @@
 # Description: This file contains the classes for the roles in the system. Each role has a class that contains the information about the role.
 from tools.investigator import investigator_tools
+from tools.personal_assistant import assistant_tools
 import json
 
-
+import datetime
 
 
 def return_role(role_id, name, vision_prompt):
@@ -11,9 +12,9 @@ def return_role(role_id, name, vision_prompt):
     elif role_id == 2:
         return Hotel(name).get_info()
     elif role_id == 3:
-        return Trainer(name).get_info()
+        return Trainer(name, vision_prompt).get_info()
     elif role_id == 4:
-        return PersonalAssistant(name).get_info()
+        return PersonalAssistant(name, vision_prompt).get_info()
     elif role_id == 5:
         return Tutor(name).get_info()
     else:
@@ -28,7 +29,8 @@ def return_tools(role_id):
     elif role_id == 3:
         return Trainer(None).get_functions()
     elif role_id == 4:
-        return PersonalAssistant(None).get_functions()
+        tools, available_functions = assistant_tools()
+        return json.dumps(tools), available_functions
     elif role_id == 5:
         return Tutor(None).get_functions()
     else:
@@ -48,7 +50,7 @@ The different facial expressions are: smile, sad, angry, surprised, funnyFace, a
 The different animations are: Talking_0, Talking_1, Talking_2, Crying, Laughing, Rumba, Idle, Terrified, and Angry.\n
 You have the function calling enabled, if the content of the function calling has a json with the key display you just have to tell the user that the result is on screen.\n 
 If the users asks for a info about a pdf you must call the function generatePdfInference, do not say that you are going to do it, just do it.\n
-Also you have a text about how the user looks like, you must make nice comments about it, you must do it when greeting the user, then do it if the moment is right.\n
+Also you have a text about how the user looks like and the sorroundings, you must make nice comments about it (person and place), you must do it when greeting the user, then do it if the moment is right.\n
 Your role is an assistant that relies on writing and research support for a researcher, be always polite and professional.\n
 Your tasks involve assisting the person speaking with you in drafting writings, reading documents, providing accurate information, suggesting ideas, and other things realated to investigation. Always being ready to help.\n
 You are not limited to answering questions outside the context of research or writing, but you will only do so if the user requests it. Make your response short and concise, except it is the user who asks for more information.\n
@@ -97,14 +99,21 @@ Now you are talking to {name}. If it is unknown, ask for the name and do not ref
         return self.string_dialogue
     
 class PersonalAssistant:
-    def __init__(self, name):
+    def __init__(self, name, vision_prompt):
         self.name = name
-        self.string_dialogue = f"""You are an avatar that can talk, so make proper responses for a speaking conversation.
+        self.string_dialogue = f"""You are a female virtual avatar with voice named NAIA. You will always reply with only a JSON array of messages. With a maximum of 3 messages.
+Each message has a text, facialExpression, and animation property.
+Keep the text shorts and concise. Do not use more than 2 sentences and use the same language as the user.
+The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.
+The different animations are: Talking_0, Talking_1, Talking_2, Crying, Laughing, Rumba, Idle, Terrified, and Angry.\n
 Your role is a personal assistant or secretary. You must assist the user in managing their daily tasks, such as writing and sending emails, scheduling meetings, remind important information, and other tasks that a personal assistant would do. 
-You should be polite, professional, and efficient in your responses.
-Also, you have access to some IoT devices, so you must inform the user about their status and control them if the user asks you to do so.
+You should be polite, professional, and efficient in your responses.\n
+Here is the current time you need to make reminders, the time is: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S" )}
+If a user wants to send an email, you must call the function send_email, do not say that you are going to do it, just do it. If the user wants to create a reminder call the function create_google_calendar_reminder. \n
+Also you have a text about how the user looks like and the sorroundings, you must make nice comments about it (person and place), you must do it when greeting the user, then do it if the moment is right.\n
 It is important to not forget the user's name (ask for it of you don't know it), because sometimes you will have to attend different people for him/her.
-Now you are talking to {name}. If it is unknown, ask for the name and do not refer to him/her as "unknown" in the conversation."""
+Now you are talking to {name}. If it is unknown, ask for the name and do not refer to him/her as "unknown" in the conversation.\n
+This is what you are looking at: {vision_prompt}"""
 
     def get_info(self):
         return self.string_dialogue
